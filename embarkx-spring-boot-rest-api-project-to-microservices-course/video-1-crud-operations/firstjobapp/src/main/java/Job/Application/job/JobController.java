154784b42e27954346bae62813968946d1071c2b
James Leone <jsalleo13@gmail.com>
2026-04-05 23:13:50 -0400

    Added JobService implementation and refactored the controller to use this service

--- embarkx-spring-boot-rest-api-project-to-microservices-course/video-1-crud-operations/firstjobapp/src/main/java/Job/Application/job/JobController.java
+++ embarkx-spring-boot-rest-api-project-to-microservices-course/video-1-crud-operations/firstjobapp/src/main/java/Job/Application/job/JobController.java
@@ -1,25 +1,36 @@
 package Job.Application.job;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JobController {
-    private List<Job> jobs = new ArrayList<>();
+    private JobService jobService;
+    private Long nextId = 1L;
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
 
     @GetMapping("/jobs")
     public List<Job> findAll() {
-        return jobs;
+        return jobService.findAll();
     }
 
     @PostMapping("/jobs")
     public String createJob(@RequestBody Job job) {
-        jobs.add(job);
+        job.setId(nextId++);
+        jobService.createJob(job);
         return "Job added successfully";
+    }
+
+    @GetMapping("/jobs/{id}")
+    public Job getJobById(@PathVariable Long id) {
+        Job job = jobService.getJobById(id);
+        return job;
     }
 }
